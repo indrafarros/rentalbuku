1569df66c88bc29333fcbe89cafbbfc1e683b074
indrafarros <indrafarros.fi@gmail.com>
2023-02-07 18:16:00 +0700

    add search book

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,13 +3,22 @@
 namespace App\Http\Controllers;
 
 use App\Models\Book;
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $book = Book::all();
-        return view('home', ['book' => $book]);
+        $categories = Category::all();
+
+        if ($request->category || $request->search) {
+            $book = Book::where('title', 'LIKE', '%' . $request->search . '%')->orWhereHas('categories', function ($q) use ($request) {
+                $q->where('categories.id', $request->category);
+            })->get();
+        } else {
+            $book = Book::all();
+        }
+        return view('home', ['book' => $book, 'categories' => $categories]);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,6 +5,27 @@
 @section('content')
     <h2>Welcome</h2>
 
+    <form action="" method="get">
+        <div class="row">
+            <div class="col-12 col-sm-6">
+                <select name="category" id="category" class="form-select">
+                    <option value="">Select category</option>
+                    @foreach ($categories as $val)
+                        <option value="{{ $val->id }}"> {{ $val->name }}</option>
+                    @endforeach
+                </select>
+            </div>
+            <div class="col-12 col-sm-6">
+                <div class="input-group mb-3">
+                    <input type="text" name="search" id="search" class="form-control"
+                        placeholder="Search book's title" name="title">
+                    <button class="btn btn-primary" type="submit">Search</button>
+
+                </div>
+            </div>
+        </div>
+    </form>
+
     <div class="my-5">
         <div class="row">
             @foreach ($book as $item)
@@ -23,7 +44,6 @@
                     </div>
                 </div>
             @endforeach
-
         </div>
     </div>
 @endsection
